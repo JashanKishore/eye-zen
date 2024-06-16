0e9f18215afb4d9bf4a9ab032764d0dcddd9662e
Jashan Kishore <jashan.kishore@gmail.com>
2024-06-16 16:51:33 +1000

    Initial Commit

---
+++ eye-zen/eye_zenApp.swift
@@ -0,0 +1,17 @@
+//
+//  eye_zenApp.swift
+//  eye-zen
+//
+//  Created by Jashan kishore on 16/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct eye_zenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
