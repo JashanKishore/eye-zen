c691d568d872f890c5c675b79e889d05503be476
Jashan Kishore <jashan.kishore@gmail.com>
2024-07-01 15:32:20 +1000

    Added circular progress view for counter

---
+++ eye-zen/CircularProgressView.swift
@@ -0,0 +1,36 @@
+//
+//  CircularProgressView.swift
+//  eye-zen
+//
+//  Created by Jashan kishore on 1/7/2024.
+//
+
+import SwiftUI
+
+struct CircularProgressView: View {
+    var progress: CGFloat
+    var lineWidth: CGFloat = 10.0
+    var circleColor: Color = .blue
+    var backgroundColor: Color = .gray.opacity(0.3)
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(backgroundColor, lineWidth: lineWidth)
+            
+            Circle()
+                .trim(from: 0.0, to: progress)
+                .stroke(circleColor, lineWidth: lineWidth)
+                .rotationEffect(.degrees(-90))
+                .animation(.linear)
+        }
+    }
+}
+
+struct CircularProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircularProgressView(progress: 0.7)
+            .frame(width: 100, height: 100)
+    }
+}
+
